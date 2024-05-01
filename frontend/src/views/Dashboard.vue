a3fa2baec19fa8bfcdbbb1b4d10124c16917fab4
edmundlemon <edmundngxhunyan@gmail.com>
2024-05-01 17:20:58 +0800

    added frontend file

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<h1>Dashboard</h1>
+		<table>
+			<tr>
+				<td>Username</td>
+				<td>{{ user.name }}</td>
+			</tr>
+			<tr>
+				<td>Email</td>
+				<td>{{ user.email }}</td>
+			</tr>
+		</table>
+	</div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useStore } from 'vuex'
+
+
+const store = useStore()
+const user = store.state.user.data
+
+onMounted(() => {
+	// const user = store.commit('getAll')
+	console.log(store.state.user.data)
+	// console.log(user)
+})
+</script>
+
+<style>
+
+</style>
