eb59099ad3799b28fe2c857924c527ce646a79e9
YHH <yaphoonghey@gmail.com>
2024-05-28 01:37:34 +0800

    login page yhh

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { defineProps } from 'vue'
 import Header from './components/Header.vue'
+import store from './store'
 </script>
 
 <template>
   <div>
-    <Header />
+    <Header v-if="store.state.token"/>
     <router-view></router-view>
   </div>
 </template>
