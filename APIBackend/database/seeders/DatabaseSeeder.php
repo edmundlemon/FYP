f4135f10ca28d9ca19a5ca70c1d1238499c84362
YHH <yaphoonghey@gmail.com>
2024-06-05 01:31:35 +0800

    added some users to seeder in backend

--- APIBackend/database/seeders/DatabaseSeeder.php
+++ APIBackend/database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Student::create([
-            'id' => "1122",
+            'id' =>"1122",
             'name' => 'John Doe',
             'email' => '1122@gmail.com',
             'password' => bcrypt('password'),
@@ -45,7 +45,7 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('password'),
             'photo' => 'https://via.placeholder.com/150',
             'faculty' => 'FCI',
-            'office' => 'BR-4013'
+            'office' => 'A-123',
         ]);
         Lecturer::create([
             'id' => 'MU1212',
@@ -53,43 +53,35 @@ class DatabaseSeeder extends Seeder
             'email' => 'mu1212@gmail.com',
             'password' => bcrypt('password'),
             'photo' => 'https://via.placeholder.com/150',
-            'faculty' => 'FCI'
-        ]);
-        Lecturer::create([
-            'id' => 'MU4321',
-            'name' => 'Joe Mumma',
-            'email' => 'mumma1@gmail.com',
-            'password' => bcrypt('password'),
-            'photo' => 'https://via.placeholder.com/150',
-            'faculty' => 'FOL',
-            'office' => 'BR-3222'
+            'faculty' => 'FCI',
+            'office' => 'A-133',
         ]);
         Lecturer::create([
             'id' => 'MU2233',
-            'name' => 'Deez NUTS',
-            'email' => 'deez2@gmail.com',
-            'password' => bcrypt('password'),
-            'photo' => 'https://via.placeholder.com/150',
-            'faculty' => 'FOM',
-            'office' => 'BR-2032'
-        ]);
-        Lecturer::create([
-            'id' => 'MU3344',
-            'name' => 'Zane Malik',
-            'email' => 'zanny@gmail.com',
-            'password' => bcrypt('password'),
-            'photo' => 'https://via.placeholder.com/150',
-            'faculty' => 'FCM',
-            'office' => 'BR-4322'
-        ]);
-        Lecturer::create([
-            'id' => 'MU4455',
-            'name' => 'Edmund Lemon',
-            'email' => 'edlm@gmail.com',
+            'name' => 'Jane Bye',
+            'email' => 'mu2233@gmail.com',
             'password' => bcrypt('password'),
             'photo' => 'https://via.placeholder.com/150',
             'faculty' => 'FOE',
-            'office' => 'CR-3242'
+            'office' => 'B-123',
+        ]);
+        Lecturer::create([
+            'id' => 'MU3322',
+            'name' => 'Ray Bye',
+            'email' => 'zaza@gmail.com',
+            'password' => bcrypt('password'),
+            'photo' => 'https://via.placeholder.com/150',
+            'faculty' => 'FCM',
+            'office' => 'C-123',
+        ]);
+        Lecturer::create([
+            'id' => 'MU4433',
+            'name' => 'Kai Cenat',
+            'email' => 'kc2233@gmail.com',
+            'password' => bcrypt('password'),
+            'photo' => 'https://via.placeholder.com/150',
+            'faculty' => 'FCA',
+            'office' => 'C-223',
         ]);
         Admin::create([
             'id' => 'AD1234',
@@ -100,29 +92,14 @@ class DatabaseSeeder extends Seeder
         Review::create([
             'id' => 1,
             'lecturer_id' => 'MU1234',
-            'student_id' => "1122",
+            'student_id' =>"1122",
             'rating' => '5',
-            'comment' => 'Great'
-        ]);
-        Lecturer::create([
-            'id' => 'MU1212',
-            'name' => 'Jane Hello',
-            'email' => 'mu1212@gmail.com',
-            'password' => bcrypt('password'),
-            'photo' => 'https://via.placeholder.com/150',
-            'faculty' => 'FCI'
-        ]);
-        Review::create([
-            'id' => 2,
-            'lecturer_id' => 'MU1234',
-            'student_id' => "1122",
-            'rating' => '3',
             'comment' => 'Great'
         ]);
         Consultation_slot::create([
             // 'id' => 1,
             'lecturer_id' => 'MU1234',
-            'student_id' => "1122",
+            'student_id' =>"1122",
             'date' => Carbon::today()->addDays(2),
             'start_time' => Carbon::now()->addMinutes(30)->format('H:i'),
             'end_time' => Carbon::now()->addMinutes(90)->format('H:i'),
@@ -131,7 +108,7 @@ class DatabaseSeeder extends Seeder
         Consultation_slot::create([
             // 'id' => 2,
             'lecturer_id' => 'MU1234',
-            'student_id' => "1122",
+            'student_id' =>"1122",
             'date' => Carbon::today(),
             'start_time' => Carbon::now()->addMinutes(25)->format('H:i'),
             'end_time' => Carbon::now()->addMinutes(75)->format('H:i'),
@@ -140,7 +117,7 @@ class DatabaseSeeder extends Seeder
         Consultation_slot::create([
             // 'id' => 2,
             'lecturer_id' => 'MU1234',
-            'student_id' => "1122",
+            'student_id' =>"1122",
             'date' => Carbon::today()->addDay(),
             'start_time' => Carbon::now()->addMinutes(25)->format('H:i'),
             'end_time' => Carbon::now()->addMinutes(75)->format('H:i'),
